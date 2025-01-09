d4454c2372aa273b70f70d413cc559893c0b3ec8
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2025-01-09 16:13:01 +0530

    "fix createissue"

--- actions/issues .js
+++ actions/issues .js
@@ -4,7 +4,7 @@ import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 
 export async function createIssue(projectId, data) {
-    const { userId, orgId } = auth();
+    const { userId, orgId } =  await auth();
   
     if (!userId || !orgId) {
       throw new Error("Unauthorized");
