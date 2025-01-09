d5605d81366583f2052d5088883a2d4930d0432f
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2025-01-09 12:30:15 +0530

    "Get projects server action"

--- actions/organizations.js
+++ actions/organizations.js
@@ -43,3 +43,25 @@ export async function getOrganization(slug) {
 
   return organization;
 }
+
+export async function getProjects(orgId) {
+  const { userId } = auth();
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const projects = await db.project.findMany({
+    where: { organizationId: orgId },
+    orderBy: { createdAt: "desc" },
+  });
+
+  return projects;
+}
