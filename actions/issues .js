d8e1a07670001ce0af184bf7a24484575209268b
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2025-01-09 15:39:51 +0530

    "Serve action issue create"

---
+++ actions/issues .js
@@ -0,0 +1,25 @@
+"use server";
+
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+
+const issue = await db.issue.create({
+    data: {
+      title: data.title,
+      description: data.description,
+      status: data.status,
+      priority: data.priority,
+      projectId: projectId,
+      sprintId: data.sprintId,
+      reporterId: user.id,
+      assigneeId: data.assigneeId || null, // Add this line
+      order: newOrder,
+    },
+    include: {
+      assignee: true,
+      reporter: true,
+    },
+  });
+
+  return
+ 
