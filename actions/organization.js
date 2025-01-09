43c604ad773f51c77de2a44c47fc2bd01c7ce1cb
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2025-01-09 12:14:14 +0530

    "Layout for project page"

--- actions/organization.js
+++ actions/organization.js
@@ -4,7 +4,7 @@ import { db } from "@/lib/prisma";
 import { auth, clerkClient } from "@clerk/nextjs/server";
 
 export async function getOrganization(slug) {
-  const { userId } = auth();
+  const { userId } = await auth();
   if (!userId) {
     throw new Error("Unauthorized");
   }
